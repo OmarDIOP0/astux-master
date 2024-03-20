d93e651c79535df545301eb757f814eff3b3bea0
OmarDIOP0 <odiop6170@gmail.com>
2024-03-20 18:26:18 +0000

    connect_to_mongodb_using_mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
+
+dotenv.config();
 const app = express();
+app.use(express.json())
+const port =process.env.PORT || 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
 app.listen(3000, () => {
+    connectToMongoDB();
   console.log('Example app listening on port 3000!');
 });
